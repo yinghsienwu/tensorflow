019ce81dbabfa9bd164e2f6dd105c52e97f8d000
Amy Wu <amywu@Roberts-MacBook-Air-4.local>
2016-10-18 13:31:00 -0700

    feat:notMNIST

--- softmax.py
+++ softmax.py
@@ -1,14 +1,13 @@
 """Softmax."""
-
-scores=[3.0, 1.0, 0.2]
-
 import numpy as np
+
+scores=np.array([3.0, 1.0, 0.2])
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x. """
     return np.exp(x)/sum(np.exp(x))
 
-print(softmax(scores))
+print(softmax(scores/10))
 
 #Plot softmax curves
 import matplotlib.pyplot as plt
